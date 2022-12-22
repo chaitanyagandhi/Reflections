35e063caf74b291e1a0712a17a253773c240fad8
Chaitanya Gandhi <chaitanya100801@gmail.com>
2022-12-22 20:54:03 +0530

    First commit

---
+++ server/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import * as dotenv from "dotenv";
+dotenv.config();
+import postRoutes from "./routes/posts.js";
+import userRouter from "./routes/user.js";
+
+const app = express();
+
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/posts", postRoutes);
+app.use("/user", userRouter);
+
+app.get("/", (req, res) => {
+  res.send("APP IS RUNNING.");
+});
+
+const CONNECTION_URL = process.env.MONGO_URI;
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set("useFindAndModify", false);
